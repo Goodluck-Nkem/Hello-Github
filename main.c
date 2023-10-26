870f2131e3e13da424ff340ff9b3f0898b2273f4
Goodluck-Nkem <54841873+Goodluck-Nkem@users.noreply.github.com>
2023-10-26 23:13:22 +0100

    Update main.c
    Added new function called print_message().
    All of this done in GitHub, so code not yet tested.

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_message (void)
+{
+  puts("string 1");
+  if(1)
+  {
+    puts("string 2");
+  }
+}
+
 int main()
 {
   char name[1024];
@@ -8,5 +17,6 @@ int main()
   fgets(name, 1023, stdin); /* read input */
   name[strlen(name) - 1] = '\0'; /*remove trailing newline */
   printf("%s, Says Hello!\n", name);
+  print_message();
   return 0;
 }
