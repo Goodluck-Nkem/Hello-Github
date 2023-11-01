5fe4c25cbc2f80e3f95c1070f8be447dbfb45f46
Nkem <54841873+Goodluck-Nkem@users.noreply.github.com>
2023-11-01 16:50:12 +0000

    Commit test from github Online  Workspace

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-void print_message (void)
+void print_message(void)
 {
   puts("string 1");
-  if(1)
+  if (1)
   {
     puts("string 2");
   }
@@ -14,9 +14,11 @@ int main()
 {
   char name[1024];
   printf("Say your name: ");
-  fgets(name, 1023, stdin); /* read input */
-  name[strlen(name) - 1] = '\0'; /*remove trailing newline */
-  printf("%s, Says Hello!\n", name);
-  print_message();
+  if (fgets(name, 1023, stdin)) /* read input */
+  {
+    name[strlen(name) - 1] = '\0'; /*remove trailing newline */
+    printf("%s, Says Hello to you!\n", name);
+    print_message();
+  }
   return 0;
 }
